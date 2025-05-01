0f68da5a24c773480d8b1932b11e84e08cec6b59
Malshani RathnaSri <malshanirathnasri@gmail.com>
2025-05-01 23:47:04 +0530

    #2 Feat: Design Admin Panel

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -7,6 +7,10 @@ Route::get('/',function() {
     return view('frontend/Home');
 });
 
+Route::get('/test', function() {
+    return view('admin/dashboard');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
